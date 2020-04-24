22ec67166463a1bbb70e5c4b9e5a30234bdcdda6
dia006 <manuel.cecconi@hotmail.com>
2020-04-24 14:59:19 +0200

    Fixed the environment for VSCode have to end the notebooks

---
+++ 0. Latest News Classifier/01. Dataset Creation/01. Dataset Creation.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+sRootPath = "./0. Latest News Classifier/00. Raw dataset/BBC/bbc-fulltext/bbc"
+sOutput = "./0. Latest News Classifier/01. Dataset Creation/News_dataset_py.csv"
+
+aColumns = ["File_name", "Content", "Category", "Complete_Filename"]
+aRows = []
+for sCategory in os.listdir(sRootPath):
+    sPath = sRootPath + "/" + sCategory
+    for sFile in os.listdir(sPath):
+        with open(sPath + "/" + sFile, "r") as f:
+            sContent = f.read()
+        aRows.append([sFile, sContent, sCategory, sFile + "-" + sCategory])
+d_Data = pd.DataFrame(aRows, columns=aColumns)
+d_Data.to_csv(sOutput, index=False, encoding="utf-8")
